670f9128e5668947e4e14b8bd3c802cd2604c3bf
lizgar <liz17_9@hotmail.com>
2025-10-17 10:51:25 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/DesignareSemillero/',
+    base: 'https://lizgar.github.io/DesignareSemillero/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
